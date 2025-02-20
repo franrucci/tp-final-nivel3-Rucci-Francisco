e58f0ee541549ad0567888b3fb44485d20b616fe
francisco-rucci <francisco.rucci@alumnos.uai.edu.ar>
2025-02-20 17:51:11 -0300

    Navbar responsive

--- Catalogo-web/Js/JavaScript.js
+++ Catalogo-web/Js/JavaScript.js
@@ -2,3 +2,17 @@
     image.onerror = "";
     image.src = 'https://static.vecteezy.com/system/resources/previews/004/141/669/non_2x/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg';
 }
+
+function toggleMenu() {
+    var sidebar = document.getElementById("sidebarMenu");
+    var overlay = document.getElementById("overlay");
+
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+        overlay.style.display = "none";
+    } else {
+        sidebar.style.left = "0px";
+        overlay.style.display = "block";
+    }
+}
+
